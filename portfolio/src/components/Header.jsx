e2134190dfac37faa9accd6b0054793fc41ab1d6
taryncodes@github.com <taryn.bilsborough@gmail.com>
2023-10-24 14:33:50 +0100

    added pic and placement, also designed elements to be responsive

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import '../App.css'
 import TechSkills from './Tech-skills'
+import Pic from './Pic'
 
 
 
 function Header() {
   return (
     <div>
-    <TechSkills/>
+      <Pic/>
     <section className='border-main'>
     <h3 id= 'intro'>Hello!</h3>
     <h1 id='main-header'> I'm Taryn, </h1>
-    <h1 id='job-description'> A Junior Developer based in the UK</h1>
+    <h1 id='job-description'> A Junior Software Developer based in the UK. </h1>
+    <h2 id='job-description'> I enjoy all things Frontend, creating beautiful and responsive applications</h2>
     </section>
   
   </div>
---
+++ portfolio/src/components/Pic.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import myImage from '../assets/self-portrait.jpg'
+
+
+function Pic() {
+  return (
+    <img src={myImage} className='self-pic'/>
+  )
+}
+
+export default Pic
